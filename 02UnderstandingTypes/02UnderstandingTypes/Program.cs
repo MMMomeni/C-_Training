547ce3648561e114fade3c120dcf068ba7d030d0
Mohammad Momeni <68871825+MMMomeni@users.noreply.github.com>
2022-07-07 09:00:11 -0500

    Add files via upload

---
+++ 02UnderstandingTypes/02UnderstandingTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("sbyte is 1 Byte. Min: -128, Max: 127");
+Console.WriteLine("byte is 1 Byte. Min: 0, Max: 255");
+Console.WriteLine("short is 2 Bytes. Min: -32768, Max: 32768");
+Console.WriteLine("ushort is 2 Bytes. Min: 0, Max: 65535");
+Console.WriteLine("int is 4 Bytes. Min: -2147483648, Max: 2147483647");
+Console.WriteLine("uint is 4 Bytes. Min: 0, Max: 4294967295");
+Console.WriteLine("long is 8 Bytes. Min: -9223372036854775808, Max: 9223372036854775807");
+Console.WriteLine("ulong is 8 Bytes. Min: 0, Max: 18446744073709551615");
+Console.WriteLine("float is 4 Bytes. Min: -3.402823E+38, Max: 3.402823E+38");
+Console.WriteLine("double is 8 Bytes. Min: -1.79769313486232E+308, Max: 1.79769313486232E+308");
+Console.WriteLine("decimal is 16 Bytes. Min: -79228162514264337593543950335, Max: 79228162514264337593543950335");
---
+++ CenturyToYear/CenturyToYear/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter a century: ");
+String input = Console.ReadLine();
+int century = Int32.Parse(input);
+int years = century * 100;
+int days = years * 365;
+int hours = days * 24;
+int minutes = hours * 60;
+int seconds = minutes * 60;
+int miliseconds = seconds * 1000;
+int microseconds = miliseconds * 1000;
+int nanoseconds = microseconds * 1000;
+Console.WriteLine($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {miliseconds} miliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+
+
+
